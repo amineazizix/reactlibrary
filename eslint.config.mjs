50e96ace1e3141c5eb44bf13bd44f2118b89d8e5
Amine Azizi <amineazizix@Amines-MacBook-Pro.local>
2024-09-24 16:22:58 +0100

    feat: adding conventional commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,6 +7,9 @@ import pluginReact from "eslint-plugin-react";
 export default [
   {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
   {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
   {
     rules: {
       "react/react-in-jsx-scope": "off",
@@ -18,7 +21,4 @@ export default [
       },
     },
   },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
 ];
