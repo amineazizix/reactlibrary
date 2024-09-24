5eaace18c8c7e363b3af1d6c3977ae8a2dd7cbf0
Amine Azizi <amineazizix@Amines-MacBook-Pro.local>
2024-09-24 18:32:44 +0100

    ci: adding circleci pipline

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,7 +6,13 @@ import pluginReact from "eslint-plugin-react";
 
 export default [
   {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  {languageOptions: { 
+    globals: globals.browser,
+    parserOptions: {
+      ecmaVersion: "latest",
+      sourceType: "script",
+    },
+  }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
